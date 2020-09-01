178bf898c3f39928867145ae98e323ec7ecf4059
camilla-design <56583550+camilla-design@users.noreply.github.com>
2020-09-01 10:06:56 +0200

    error

---
+++ tailwindcss/tailwindapp/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        'card-heading': '#0e1726'
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
